4183f2066de18a31c4ba65853633c1d06a8ff63c
syztem <syztem@syztems-MacBook-Pro.local>
2020-09-14 13:26:24 -0400

    Removed Client Class from client.py, implement default main() to run instead

--- cnt4713/client.py
+++ cnt4713/client.py
@@ -4,54 +4,40 @@
 # Class: CNT4713 RVC 1208
 
 import socket, sys, os
+        
+def main():
+    args = sys.argv[1:]
 
-class Client:
-    def __init__(self, sock=None, timeout=10):
-        if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     
-            self.sock.settimeout(timeout)
-        else:
-            self.sock = sock
+    hostname = args[0]
+    port = int(args[1])
+    filename = args[2]
 
-    def connect(self, host, port):
+    file = open(filename, "rb")
+    data = file.read(os.path.getsize(filename))
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         if(port in range(0, 65535)):
-            serverAddress = (host, port)
+            s.connect((hostname, port))
+            s.settimeout(10)
         else:
             sys.stderr.write("ERROR: (port must be 0-65535.)")
             exit(1)
         
-        try:
-            self.sock.connect(serverAddress)
-        except socket.error:
-            sys.stderr.write("ERROR: (Error: connection could not be stablished.)")
-            exit(1)
+        recv = s.recv(16)
+        if (recv == "accio\r\n"):
+            s.sendall(data)
 
-    def receiveMessage(self, size):
-        try:
-            return self.sock.recv(size).decode('utf-8')
-        except socket.timeout:
-            sys.stderr.write("ERROR: (Error: timed out)")
-            exit(1)
+    except socket.error:
+        sys.stderr.write("ERROR: (Error: connection could not be stablished.)")
+        exit(1)
+    except socket.timeout:
+        sys.stderr.write("ERROR: (Error: timed out)")
+        exit(1)
+    finally:
+        file.close()
+        s.close()
 
-    def sendMessage(self, msg):
-        self.sock.sendall(msg)
-        
-args = sys.argv[1:]
-
-hostname = args[0]
-port = int(args[1])
-filename = args[2]
-
-file = open(filename,  'rb')
-contents = file.read(os.path.getsize(filename))
-
-client = Client()
-client.connect(hostname, port)
-    
-recv = client.receiveMessage(16)
-
-if (recv == 'accio\r\n'):
-    client.sendMessage(contents)
-
-client.sock.close()
-exit(0)
+if __name__ == "__main__":
+    main()
