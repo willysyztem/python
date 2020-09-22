2a72639da505582fc469c093bce4e9ec7e8636a4
William Valido <valido.william@gmail.com>
2020-09-22 03:26:46 -0400

    added functions to client.py, server-s.py

--- cnt4713/client.py
+++ cnt4713/client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 global host
 global port
--- cnt4713/server-s.py
+++ cnt4713/server-s.py
@@ -1,47 +1,77 @@
-# William Valido
-# 3354913
-# Professor: Alex Afanasyev
-# Class: CNT4713 RVC 1208
-# Accio Simplified Server
+import socket
+import sys
 
-import socket, sys, time
+global host
+global port
+global s
+
+
+def socketCreate():
+    global host
+    global port
+    global s
+
+    host = "0.0.0.0"
+    port = int(sys.argv[1])
+
+    # checks to see if the port is within allowed range
+    if port not in range(0, 65535):
+        sys.stderr.write("Error: (port number not in range)")
+        sys.exit(1)
+    s = socket.socket()
+
+
+def socketBind():
+    global host
+    global port
+    global s
+
+    s.bind((host, port))
+    s.listen(10)
+
+
+def socketAccept():
+    connection, address = s.accept()
+    connection.settimeout(10.0)
+
+    socketSend(connection)
+    socketRecv(connection)
+    connection.close()
+
+
+def socketSend(connection):
+    # only accio\r\n is send
+    connection.send(str.encode("accio\r\n"))
+
+
+def socketRecv(connection):
+    totalData = 0
+    while True:
+        data = len(connection.recv(1024))
+        if not data:
+            break
+        totalData += data
+    print(totalData)
+
 
 def main():
+    socketCreate()
+    socketBind()
 
-    port = int(sys.argv[1])
     allowedConnections = 10
     totalConnected = 0
 
-    if(port not in range(0, 65535)):
-        sys.stderr.write("ERROR: (port must be 0-65535.)")
-        exit(1)
-    
-    s = socket.socket()
-    s.bind(("0.0.0.0", port))
-    s.listen(10)
-    
     while totalConnected < allowedConnections:
-        #print("Waiting for connection...")
         try:
-            totalData = 0
-            connection, address = s.accept()
-            #print("Connected to {} on port {}".format(address[0], address[1]))
-            connection.settimeout(10)
-
-            connection.send(str.encode("accio\r\n"))
-            
-            while True:
-                data = len(connection.recv(1024))
-                if not data:
-                    break
-                totalData += data
-
-            print(totalData)
-            connection.close()
+            socketAccept()
         except socket.timeout:
-            sys.stderr.write("ERROR")
+            sys.stderr.write("Error\n")
         totalConnected += 1
-        
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        main()
+    else:
+        sys.stderr.write("Run with arguments >> python server-s.py <PORT>")
+        sys.exit(1)
